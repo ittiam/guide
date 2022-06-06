878e6ed8f23a3ceada98b424d90e4d537aeab605
chonger <ittiamc@gmail.com>
2022-06-06 21:10:24 +0800

    table

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -32,6 +32,7 @@ module.exports = {
         '/docs/common/design',
         '/docs/common/question',
         '/docs/common/markdown',
+        '/docs/common/code-review',
         '/docs/common/api',
         {
           title: 'HTML 规范',
